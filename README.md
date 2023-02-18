6ebaf3504a65c5c1a56a015609f5bc10c50e62b1
Bismo Baruno <bismobaruno@gmail.com>
2023-02-18 13:42:25 +0900

    feat: add split numbers

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Just for my personal collections.
 
 1. Digit to String - [main.go](digit-to-str/main.go)
 2. Numbers to String - [main.go](numbers-to-str/main.go)
-3. String to Digit - [main.go](str-to-digit/main.go)
-4. String to Numbers - [main.go](str-to-numbers/main.go)
+3. Split Number - [main.go](split-numbers/main.go)
+4. String to Digit - [main.go](str-to-digit/main.go)
+5. String to Numbers - [main.go](str-to-numbers/main.go)
--- split-numbers/main.go
+++ split-numbers/main.go
@@ -5,30 +5,27 @@ import "fmt"
 func main() {
 	// only positive numbers without leading zero
 	// 012345, -12345 -> not acceptable
-	numbers := 12345
+	n := 12345
 
-	// prepare the array of rune
-	var str []rune
+	// prepare the array of numbers
+	var numbers []int
 
 	// iterates until numbers become 0
-	for numbers != 0 {
+	for n != 0 {
 		// use modulo 10 in order to get every digit
 		// e.g. 12345 % 10 -> 5
-		m := numbers % 10
-
-		// convert to rune string
-		r := '0' + rune(m)
+		m := n % 10
 
 		// use prepend because we need to add
 		// digit from right to left
 		// [5] -> [4 5] -> [3 4 5]
-		str = append([]rune{r}, str...)
+		numbers = append([]int{m}, numbers...)
 
 		// divide the number to eliminate the last digit
 		// 12345 to 1234
-		numbers /= 10
+		n /= 10
 	}
 
 	// print
-	fmt.Println(string(str))
+	fmt.Println(numbers)
 }
